df92215e51469c6274e8ca727474fb76cbd83457
bartholomews <git@bartholomews.io>
2020-05-20 21:18:02 +0100

    SpotifyClient

---
+++ src/main/scala/io/bartholomews/spotify4s/SpotifyClient.scala
@@ -0,0 +1,42 @@
+package io.bartholomews.spotify4s
+
+import cats.effect.{ContextShift, IO}
+import io.bartholomews.fsclient.client.FsClientV2
+import io.bartholomews.fsclient.config.{FsClientConfig, UserAgent}
+import io.bartholomews.fsclient.entities.oauth.v2.OAuthV2AuthorizationFramework.ClientPassword
+import io.bartholomews.fsclient.entities.oauth.{ClientPasswordBasicAuthenticationV2, SignerV2}
+import io.bartholomews.spotify4s.api._
+import pureconfig.ConfigSource
+
+import scala.concurrent.ExecutionContext
+
+class SpotifyClient(client: FsClientV2[IO, SignerV2])(
+  implicit ec: ExecutionContext
+) {
+  private implicit val contextShift: ContextShift[IO] = IO.contextShift(ec)
+
+  object auth extends AuthApi(client)
+  object browse extends BrowseApi(client)
+  object playlists extends PlaylistsApi(client)
+  object tracks extends TracksApi(client)
+  object users extends UsersApi(client)
+}
+
+object SpotifyClient {
+  import FsClientConfig.LoadConfigOrThrow
+  import pureconfig.generic.auto._
+
+  def unsafeFromConfig()(implicit ec: ExecutionContext, cs: ContextShift[IO]): SpotifyClient =
+    (for {
+      userAgent <- ConfigSource.default.at(namespace = "user-agent").load[UserAgent]
+      clientPassword <- ConfigSource.default
+                         .at("spotify")
+                         .load[ClientPassword]
+    } yield
+      new SpotifyClient(
+        new FsClientV2(
+          appConfig = FsClientConfig(userAgent, ClientPasswordBasicAuthenticationV2(clientPassword)),
+          clientPassword
+        )
+      )).orThrow
+}
---
+++ src/test/scala/io/bartholomews/spotify4s/SpotifyClientSpec.scala
@@ -0,0 +1,23 @@
+package io.bartholomews.spotify4s
+
+import io.bartholomews.spotify4s.client.ClientData._
+import io.bartholomews.testudo.WireWordSpec
+
+class SpotifyClientSpec extends WireWordSpec {
+  "SpotifyClient" when {
+    "initialised with an implicit configuration" should {
+      "read the consumer values from resource folder" in {
+        noException shouldBe thrownBy {
+          SpotifyClient.unsafeFromConfig()
+        }
+      }
+    }
+
+    "initialised with an explicit configuration" should {
+      "read the consumer values from the injected configuration" in {
+        val client = sampleClient
+        client shouldBe a[SpotifyClient]
+      }
+    }
+  }
+}
