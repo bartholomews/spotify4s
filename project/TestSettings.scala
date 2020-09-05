668d9a34c2dba494498d3ec465daccbd89e933a8
bartholomews <github@bartholomews.io>
2020-09-05 13:20:43 +0100

    test-all -> test-coverage

--- project/TestSettings.scala
+++ project/TestSettings.scala
@@ -2,6 +2,6 @@ import sbt.{addCommandAlias, Def}
 
 object TestSettings {
   def apply(): Def.SettingsDefinition =
-    addCommandAlias("test-all", ";scalafmtCheckAll ;coverage ;test ;coverageReport") ++
+    addCommandAlias("test-coverage", ";coverage ;test ;coverageReport") ++
       addCommandAlias("test-fast", "testOnly * -l org.scalatest.tags.Slow")
 }
