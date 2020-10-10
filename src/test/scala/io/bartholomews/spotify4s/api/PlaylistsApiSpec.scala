46fcd1f264c336776b0a2f099261259ef86c8895
bartholomews <github@bartholomews.io>
2020-10-10 23:32:59 +0100

    fmt

--- src/test/scala/io/bartholomews/spotify4s/api/PlaylistsApiSpec.scala
+++ src/test/scala/io/bartholomews/spotify4s/api/PlaylistsApiSpec.scala
@@ -79,7 +79,6 @@ class PlaylistsApiSpec extends WireWordSpec with ServerBehaviours with Matchers 
         (1 to 101).map(_ => SpotifyUri("just one extra uri...")).toList
 
       "return a compile time error" in {
-
         val ex1: Either[String, SpotifyUris] = SpotifyUri.fromList(List.empty)
         assert(ex1 == Left("Predicate failed: need to provide at least one uri."))
 
@@ -89,7 +88,6 @@ class PlaylistsApiSpec extends WireWordSpec with ServerBehaviours with Matchers 
 
         val ex2: Either[String, SpotifyUris] = SpotifyUri.fromNel(asda)
         assert(ex2 == Left("Predicate failed: a maximum of 100 uris can be set in one request."))
-
 
         inside(SpotifyUri.fromList(tooManyUris)) {
           case Left(error) => error shouldBe "Predicate failed: a maximum of 100 uris can be set in one request."
